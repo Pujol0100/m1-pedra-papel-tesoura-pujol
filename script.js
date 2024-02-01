6ac323af98e389e1dfda67d03cdf2db47fefadf5
Vinicius Pujol A. M. Piccirilli <vinicius.pujol@yahoo.com>
2024-02-01 20:47:02 -0300

    Projeto concluído

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    
+    if (!["Pedra", "Papel", "Tesoura"].includes(player1)) {
+    }
+
+    if (!["Pedra", "Papel", "Tesoura"].includes(player2)) {
+    }
+  
+    const regras = {
+      Pedra: {
+        vence: "Tesoura",
+        perde: "Papel",
+      },
+      Papel: {
+        vence: "Pedra",
+        perde: "Tesoura",
+      },
+      Tesoura: {
+        vence: "Papel",
+        perde: "Pedra",
+      },
+    };
+  
+    if (player1 === player2) {
+      return "Empate!";
+    }
+   
+    const vencedor = regras[player1].vence === player2 ? "Jogador 1 venceu!" : "Jogador 2 venceu!";
+    
+    return vencedor;
+
+  }
